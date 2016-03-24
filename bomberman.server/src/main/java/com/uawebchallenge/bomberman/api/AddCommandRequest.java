0d341dc7d25c55cbf57de9141d1806df63a0fb05
Yevgen Kravchenko <yevgen.kravchenko@gmail.com>
2016-03-25 01:29:14 +0200

    Implemented a way to add commands

---
+++ bomberman.server/src/main/java/com/uawebchallenge/bomberman/api/AddCommandRequest.java
@@ -0,0 +1,8 @@
+package com.uawebchallenge.bomberman.api;
+
+import lombok.Data;
+
+@Data
+public class AddCommandRequest {
+    private String command;
+}
--- bomberman.server/src/main/java/com/uawebchallenge/bomberman/api/BomberController.java
+++ bomberman.server/src/main/java/com/uawebchallenge/bomberman/api/BomberController.java
@@ -3,12 +3,10 @@ package com.uawebchallenge.bomberman.api;
 import com.uawebchallenge.bomberman.game.control.GameManager;
 import com.uawebchallenge.bomberman.game.model.Game;
 import com.uawebchallenge.bomberman.game.model.Player;
+import com.uawebchallenge.bomberman.game.model.PlayerCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class BomberController {
@@ -27,5 +25,16 @@ public class BomberController {
         Player player = newGame.connectHuman();
         gameManager.startGame(newGame);
         return new NewGameResponse(newGame.getGameId(), player.getPlayerId());
+    }
+
+    @RequestMapping(path = "/game/{gameId}/player/{playerId}/command", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.OK)
+    public void addCommand(@PathVariable String gameId,
+                           @PathVariable String playerId,
+                           @RequestBody AddCommandRequest request) {
+        PlayerCommand playerCommand = PlayerCommand.getCommand(request.getCommand());
+        Game game = gameManager.getGame(gameId);
+        Player player = game.getPlayer(playerId);
+        player.setNextCommand(playerCommand);
     }
 }
