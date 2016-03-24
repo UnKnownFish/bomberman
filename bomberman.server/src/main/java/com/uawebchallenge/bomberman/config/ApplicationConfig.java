c890a6be39636a52455e34c36a787b75b81747ff
Yevgen Kravchenko <yevgen.kravchenko@gmail.com>
2016-03-25 01:28:54 +0200

    Fixed config

--- bomberman.server/src/main/java/com/uawebchallenge/bomberman/config/ApplicationConfig.java
+++ bomberman.server/src/main/java/com/uawebchallenge/bomberman/config/ApplicationConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.uawebchallenge.bomberman.game.control")
+@ComponentScan({
+        "com.uawebchallenge.bomberman.game.control",
+        "com.uawebchallenge.bomberman.api"
+})
 public class ApplicationConfig {
 }
