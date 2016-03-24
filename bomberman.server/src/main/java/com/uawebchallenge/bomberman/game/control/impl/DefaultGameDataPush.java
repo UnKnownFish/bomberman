f697e65f2bb48add03403e484d2cb8e9728bc678
Yevgen Kravchenko <yevgen.kravchenko@gmail.com>
2016-03-25 01:54:08 +0200

    Added real push

--- bomberman.server/src/main/java/com/uawebchallenge/bomberman/game/control/impl/DefaultGameDataPush.java
+++ bomberman.server/src/main/java/com/uawebchallenge/bomberman/game/control/impl/DefaultGameDataPush.java
@@ -3,6 +3,8 @@ package com.uawebchallenge.bomberman.game.control.impl;
 import com.uawebchallenge.bomberman.game.control.GameDataPush;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,10 +12,20 @@ public class DefaultGameDataPush implements GameDataPush{
 
     private final static Logger logger = LoggerFactory.getLogger(DefaultGameDataPush.class);
 
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @Autowired
+    public DefaultGameDataPush(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
     @Override
     public void push(String gameId, String data) {
         logger.info("Pushing data for gameId=" + gameId);
         logger.info(data);
         logger.info("--------------------------------------");
+
+        String topic = "/topic/" + gameId;
+        messagingTemplate.convertAndSend(topic, data);
     }
 }
