66443448c996d764fd7cf7bd1c1334b780b234f1
Yevgen Kravchenko <yevgen.kravchenko@gmail.com>
2016-03-24 08:23:59 +0200

    Fixed configs

--- bomberman.server/src/main/java/com/uawebchallenge/bomberman/Application.java
+++ bomberman.server/src/main/java/com/uawebchallenge/bomberman/Application.java
@@ -1,8 +1,6 @@
 package com.uawebchallenge.bomberman;
 
 import com.uawebchallenge.bomberman.config.ApplicationConfig;
-import com.uawebchallenge.bomberman.game.control.GameManager;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
@@ -10,9 +8,6 @@ import org.springframework.context.annotation.Import;
 @SpringBootApplication
 @Import(ApplicationConfig.class)
 public class Application {
-
-    @Autowired
-    private GameManager gameManager;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- bomberman.server/src/main/java/com/uawebchallenge/bomberman/config/ApplicationConfig.java
+++ bomberman.server/src/main/java/com/uawebchallenge/bomberman/config/ApplicationConfig.java
@@ -1,4 +1,7 @@
 package com.uawebchallenge.bomberman.config;
 
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("com.uawebchallenge.game.control")
 public class ApplicationConfig {
 }
