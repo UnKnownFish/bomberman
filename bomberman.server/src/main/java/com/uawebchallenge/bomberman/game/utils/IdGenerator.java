3c80b3beaffcc18b3449cdc2ca4dc8900d4f68e3
Yevgen Kravchenko <yevgen.kravchenko@gmail.com>
2016-03-25 14:36:10 +0200

    Generating proper IDs

--- bomberman.server/src/main/java/com/uawebchallenge/bomberman/game/utils/IdGenerator.java
+++ bomberman.server/src/main/java/com/uawebchallenge/bomberman/game/utils/IdGenerator.java
@@ -1,14 +1,14 @@
 package com.uawebchallenge.bomberman.game.utils;
 
+import java.util.UUID;
+
 public class IdGenerator {
 
     public static String gameId() {
-        // TODO Generate unique game id
-        return "1";
+        return UUID.randomUUID().toString();
     }
 
     public static String playerId() {
-        // TODO Generate unique player id
-        return "2";
+        return UUID.randomUUID().toString();
     }
 }
