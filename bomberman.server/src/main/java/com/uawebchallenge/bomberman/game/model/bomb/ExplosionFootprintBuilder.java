bf3885d6804faeeb7bc6ff8d10a2ccd9c4c92939
Yevgen Kravchenko <yevgen.kravchenko@gmail.com>
2016-03-26 17:44:04 +0200

    Implemented explosion footprint analysis

--- bomberman.server/src/main/java/com/uawebchallenge/bomberman/game/model/bomb/ExplosionFootprintBuilder.java
+++ bomberman.server/src/main/java/com/uawebchallenge/bomberman/game/model/bomb/ExplosionFootprintBuilder.java
@@ -8,15 +8,52 @@ import java.util.Optional;
 class ExplosionFootprintBuilder {
 
     public ExplosionFootprint build(int bombX, int bombY, int bombStrength, GameField gameField) {
+        int minX = findMinX(gameField, bombX, bombX - bombStrength, bombY);
+        int maxX = findMaxX(gameField, bombX, bombX + bombStrength, bombY);
+        int minY = findMinY(gameField, bombY, bombY - bombStrength, bombX);
+        int maxY = findMaxY(gameField, bombY, bombY + bombStrength, bombX);
 
-        int minX = bombX;
-        int maxX = bombX;
-        int minY = bombY;
-        int maxY = bombY;
-        int centerX = bombX;
-        int centerY = bombY;
+        return new ExplosionFootprint(minX, minY, bombX, bombY, maxX, maxY);
+    }
 
-        return new ExplosionFootprint(minX, minY, centerX, centerY, maxX, maxY);
+    private int findMinX(GameField gameField, int rangeStart, int rangeEnd, int y) {
+        for (int x = rangeStart; x >= rangeEnd; x--) {
+            Optional<GameFieldItem> fieldItem = gameField.getFieldItem(x, y);
+            if (isExplosionForbidden(fieldItem)) {
+                return x + 1;
+            }
+        }
+        return rangeEnd;
+    }
+
+    private int findMaxX(GameField gameField, int rangeStart, int rangeEnd, int y) {
+        for (int x = rangeStart; x <= rangeEnd; x++) {
+            Optional<GameFieldItem> fieldItem = gameField.getFieldItem(x, y);
+            if (isExplosionForbidden(fieldItem)) {
+                return x - 1;
+            }
+        }
+        return rangeEnd;
+    }
+
+    private int findMinY(GameField gameField, int rangeStart, int rangeEnd, int x) {
+        for (int y = rangeStart; y >= rangeEnd; y--) {
+            Optional<GameFieldItem> fieldItem = gameField.getFieldItem(x, y);
+            if (isExplosionForbidden(fieldItem)) {
+                return y + 1;
+            }
+        }
+        return rangeEnd;
+    }
+
+    private int findMaxY(GameField gameField, int rangeStart, int rangeEnd, int x) {
+        for (int y = rangeStart; y <= rangeEnd; y++) {
+            Optional<GameFieldItem> fieldItem = gameField.getFieldItem(x, y);
+            if (isExplosionForbidden(fieldItem)) {
+                return y - 1;
+            }
+        }
+        return rangeEnd;
     }
 
     private boolean isExplosionForbidden(Optional<GameFieldItem> gameFieldItemOptional) {
